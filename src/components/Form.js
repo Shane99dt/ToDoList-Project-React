267017767eded39e9c0423c15cfab04df2b5ee79
Shane99dt <dushen1999thilaksha@gmail.com>
2022-07-22 12:09:28 +0200

    conflit

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,19 +3,37 @@ import React from "react";
 class Form extends React.Component {
   constructor() {
     super();
-    
+    this.state = {
+      task: "",
+    };
   }
+
+  handleTaskDescriptionChange = (e) => {
+    this.setState(
+      {
+        task: e.target.value,
+      },
+      () => {
+        console.log(this.state.task);
+      }
+    );
+  };
+
+  // handleSubmit = (e) => {
+  //   console.log(e + );
+  // };
+
   render() {
     return (
       <>
         <form
           className="flex items-center justify-center px-4 py-3 rounded-full"
-          onSubmit={this.props.addTask}
+          onSubmit={this.handleSubmit}
         >
           <input
             type="text"
-            onSubmit={this.props.submit}
-            onChange={this.props.change}
+            onChange={this.handleTaskDescriptionChange}
+            value={this.state.task}
           />
           <button type="submit">Add task</button>
         </form>
